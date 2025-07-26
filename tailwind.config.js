4448d8d919abfd0002d92575e6db261de751abe1
frOsyt3 <nazarnfauzan@gmail.com>
2025-07-26 21:13:19 +0700

    Initial commit: Martensite landing page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#171717',
+        accent: '#FF6600',
+        textLight: '#F5F5F5',
+      },
+    },
+  },
+  plugins: [],
+} 
